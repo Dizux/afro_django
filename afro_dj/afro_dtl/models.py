c419f335ca8f2f88d936c9038ccbabca5e736b76
Dizux <rukuddez@gmail.com>
2023-04-14 11:24:59 +0300

    Worked on Login and Adding Users to Template

--- afro_dj/afro_dtl/models.py
+++ afro_dj/afro_dtl/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class UserAccount(models.Model):
+    username = models.CharField(max_length=22)
+    email = models.CharField(max_length=30)
+    gender = models.CharField(max_length=6)
+    password = models.CharField(max_length=22)
+    signuptime = models.DateTimeField(auto_now=True)
--- afro_dj/afro_dtl/views.py
+++ afro_dj/afro_dtl/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+#this auth module helps us authenticate on any model of ours.
+#i.e username and password. so we dont have to recreate djangos authentication methods
+from django.contrib.auth import authenticate, login, logout
+from .models import UserAccount
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def index(request):
     pr_title = 'Afro-Django'
-    username = 'Mariam'
-    gender = 'Female'
+    username = 'Desire'
+    gender = 'Male'
     return render(
         request,
         'index.html', 
@@ -13,3 +19,22 @@ def index(request):
 
 def register(request):
     return render(request, 'register.html')
+
+def registration(request):
+    print(request)
+    user_name = request.POST['username']
+    email = request.POST['user_email']
+    password = request.POST['password']
+    gender = request.POST['gender']
+    userdetails = [user_name, email, password, gender]
+    print(userdetails)
+    if User.objects.filter(username=user_name).first():
+        print("Username already exists")
+        #returning to index for now, we havsent created a login page yet
+        return render(request, 'index.html')
+    else:
+        user = User.objects.create_user(user_name, email, password)
+        return render(request, 'login.html')
+
+def login(request):
+    return render(request, 'login.html')
